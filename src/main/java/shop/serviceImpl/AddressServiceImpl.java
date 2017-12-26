919e993f785593ce7bf899cf7ac3334ecf1349e3
snn37428 <snn_snnn@163.com>
2017-12-26 23:42:59 +0800

    增加接口地址管理

---
+++ src/main/java/shop/serviceImpl/AddressServiceImpl.java
@@ -0,0 +1,13 @@
+package shop.serviceImpl;
+
+import shop.service.AddressService;
+
+public class AddressServiceImpl implements AddressService {
+
+    private
+
+    @Override
+    public int insertAddress() {
+        return 0;
+    }
+}
